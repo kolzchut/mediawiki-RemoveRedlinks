94c1a63f4a0aa8a08b8e4d4249b502dd8df9dbab
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2015-05-10 02:46:05 +0300

    Add basic i18n, README, COPYING

--- RemoveRedlinks.php
+++ RemoveRedlinks.php
@@ -3,13 +3,14 @@
 /**
  * Extension:RemoveRedlinks - Hide all redlinks and turn them into
  * their static text.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
  * @author Chad Horohoe <innocentkiller@gmail.com>
+ * @author Dror S. [FFS]
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
  
@@ -18,30 +19,39 @@ $wgExtensionCredits['other'][] = array(
 	'name'			=> 'RemoveRedlinks',
 	'description'	=> 'Removes all redlinks from page output for certain groups',
 	'url'			=> 'https://mediawiki.org/wiki/Extension:RemoveRedlinks',
-	'version'		=> '2.0',
-	'author'		=> array( '[mailto:innocentkiller@gmail.com Chad Horohoe]', 'Dror S. ([http://www.kolzchut.org.il Kol-Zchut])' ),
-	'license-name'	=> 'GPL-2.0+'
+	'version'		=> '2.0.1',
+	'author'		=> array(
+		'[mailto:innocentkiller@gmail.com Chad Horohoe]',
+		'Dror S. [FFS] ([http://www.kolzchut.org.il Kol-Zchut])'
+	),
+	'license-name'	=> 'GPL-2.0+',
+	'descriptionmsg' => 'ext-removeredlinks-desc',
+
 );
 
 // Groups that should still see red links
 $wgRemoveRedLinksExemptGroups = array();
 
-// Hook Registering
+// i18n
+$GLOBALS['wgMessagesDirs']['RemoveRedlinks'] = __DIR__ . '/i18n';
+
+// Hooks
 $wgHooks['LinkEnd'][] = 'RemoveRedLinks::onLinkEnd'; // Cancel red links for some users
 $wgHooks['PageRenderingHash'][] = 'RemoveRedLinks::onPageRenderingHash';
+
 
 class RemoveRedLinks {
 
 	function onPageRenderingHash( &$confstr, User $user, $optionsUsed ) {
 		global $wgRemoveRedLinksExemptGroups;
 
-		if( empty( $wgRemoveRedLinksExemptGroups ) ) {
+		if ( empty( $wgRemoveRedLinksExemptGroups ) ) {
 			return true;
 		}
 
 		$userGroups = $user->getEffectiveGroups(true);
 		$match = array_intersect( $userGroups, $wgRemoveRedLinksExemptGroups );
-		if( !empty( $match ) ) {
+		if ( !empty( $match ) ) {
 			$confstr .= "!showRedLinks";
 		}
 
@@ -54,7 +64,7 @@ class RemoveRedLinks {
 		// return possibly if our group is exempt from this
 		$userGroups = $wgUser->getEffectiveGroups(true);
 		$match = array_intersect( $userGroups, $wgRemoveRedLinksExemptGroups );
-		if( !empty( $match ) ) {
+		if ( !empty( $match ) ) {
 			return true;
 		}
 
